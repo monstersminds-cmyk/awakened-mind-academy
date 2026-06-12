4e2344ead56aced7fab8c71dcc0e9b876ac1c5a0
Walter Njau Kungu <monstersminds@gmail.com>
2026-06-12 20:47:25 +0300

    fix supabase conflict on profile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// Load Supabase once for all pages
+var SUPA_URL = 'https://ffpppcbegvjskeujlqif.supabase.co';
+var SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmcHBwY2JlZ3Zqc2tldWpscWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExODM3MTQsImV4cCI6MjA5Njc1OTcxNH0.FZu3uNtxJ8KeYx-c2d79YC3ThKzC98W4Azaxzsoot2A';
+
+var db = window.supabase.createClient(SUPA_URL, SUPA_KEY);
+
 // Mobile menu
 var hamburger = document.querySelector('.hamburger');
 var navLinks = document.querySelector('.nav-links');
